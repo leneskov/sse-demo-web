3223453de58a08fddfbfcb180fe936e6bce62146
Lene Skov <lene.skov@storytel.com>
2020-11-06 12:06:43 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface Stock {
+  name: string;
+  price: number;
+}
+
+interface StockTransaction {
+  user: string;
+  stock: Stock;
+  when: Date;
+}
+
+const useEventSource = (url: string) => {
+  const [data, updateData] = React.useState<StockTransaction>();
+  React.useEffect(() => {
+    const source = new EventSource(url);
+    source.onmessage = function logEvents(event) {
+      updateData(JSON.parse(event.data));
+    }
+  }, [url]);
+
+  return data;
+}
+
 function App() {
+
+  const data = useEventSource("http://localhost:8080/stock/transaction");
+  if(!data) {
+    return (
+      <div className="App">
+      <p>SSE-demo</p>
+    </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>SSE-demo</p>
+    <p>Latest stock update for user {data.user} as of {data.when}</p>
+    <p>Name of stock: {data.stock.name}, price: {data.stock.price}</p>
     </div>
   );
 }
